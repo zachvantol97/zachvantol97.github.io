de05636c8896875cd96736d3d8c575dc22083e0f
geomin76 <geom@vt.edu>
2024-05-08 11:04:38 -0500

    Adding cname and updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const Index = () => (
             Welcome to Happenstance!
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
-          The best connections happen by chance.
+            The best connections happen by chance.
           </p>
         </div>
         <div className="lg:w-1/2">
@@ -42,15 +42,15 @@ const Index = () => (
         <div className="lg:pr-32 xl:pr-48">
           <h3 className="text-3xl font-semibold leading-tight">Our Solution</h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-          Leveraging IP-based geolocation, Happenstance
-          connects professionals who cross paths in daily life.
-          Users are notified when they encounter others on the
-          app and can choose to share a limited professional
-          profile.
+            Leveraging IP-based geolocation, Happenstance
+            connects professionals who cross paths in daily life.
+            Users are notified when they encounter others on the
+            app and can choose to share a limited professional
+            profile.
           </p>
         </div>
       }
-      secondarySlot={<img width={"100%"} src="https://img.freepik.com/free-vector/happy-woman-chatting-with-friends-online_74855-14073.jpg"/>}
+      secondarySlot={<img width={"100%"} src="https://img.freepik.com/free-vector/happy-woman-chatting-with-friends-online_74855-14073.jpg" />}
     />
     <SplitSection
       reverseOrder
@@ -59,12 +59,12 @@ const Index = () => (
           <h3 className="text-3xl font-semibold leading-tight">
           </h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-          If both parties are interested, full profiles are
-          unveiled, facilitating a seamless in-person introduction.
+            If both parties are interested, full profiles are
+            unveiled, facilitating a seamless in-person introduction.
           </p>
         </div>
       }
-      secondarySlot={<img width={"100%"} src="https://static.vecteezy.com/system/resources/previews/004/397/320/original/business-greeting-concepts-vector.jpg"/>}
+      secondarySlot={<img width={"100%"} src="https://static.vecteezy.com/system/resources/previews/004/397/320/original/business-greeting-concepts-vector.jpg" />}
     />
     <SplitSection
       primarySlot={
@@ -72,51 +72,64 @@ const Index = () => (
           <h3 className="text-3xl font-semibold leading-tight">
           </h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-          This approach enhances the quality and authenticity of
-          professional connections by embracing the
-          serendipitous nature of in-person interactions.
+            This approach enhances the quality and authenticity of
+            professional connections by embracing the
+            serendipitous nature of in-person interactions.
           </p>
         </div>
       }
-      secondarySlot={<img width={"100%"} src="https://cdn.geekwire.com/wp-content/uploads/2018/11/bigstock-Chatting-Persons-People-Group-243691576.jpg"/>}
+      secondarySlot={<img width={"100%"} src="https://cdn.geekwire.com/wp-content/uploads/2018/11/bigstock-Chatting-Persons-People-Group-243691576.jpg" />}
     />
     <section id="market" className="py-20 lg:pt-32">
       <div className="container mx-auto text-center">
-      <h3 className="text-3xl font-semibold leading-tight">Target Market</h3>
+        <h3 className="text-3xl font-semibold leading-tight">Target Market</h3>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
-        With the majority of US workers interested in switching
-        jobs in 2024 and nearly 50% of all new hires a result of
-        recruiters, Happenstance provides a valuable networking
-        platform for employers and employees alike.
+          With the majority of US workers interested in switching
+          jobs in 2024 and nearly 50% of all new hires a result of
+          recruiters, Happenstance provides a valuable networking
+          platform for employers and employees alike.
         </div>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
-        Unlike traditional professional networking apps,
-        Happenstance focuses on meaningful, in-person
-        connections, recognizing the power of chance
-        encounters in building lasting professional relationships.
+          Unlike traditional professional networking apps,
+          Happenstance focuses on meaningful, in-person
+          connections, recognizing the power of chance
+          encounters in building lasting professional relationships.
         </div>
       </div>
     </section>
     <section id="about" className="py-20 lg:pt-32">
       <div className="container mx-auto text-center">
-      <h3 className="text-3xl font-semibold leading-tight">About</h3>
-        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
-          Zachary Van Tol is the founder of
-          Happenstance, bringing 5+ years of
-          experience at the interface of higher
-          education and partnerships with the
-          public and private sectors. He holds a
-          B.S. and M.S. from Virginia Tech and is
-          a Ph.D. Candidate at Arizona State
-          University’s School of Sustainability.
-        </div>
-        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
-        Zach founded Happenstance to
-        seamlessly integrate networking into
-        everyday life, believing technology
-        should enhance, not replace,
-        in-person communication.
-        </div>
+        <h3 className="text-3xl font-semibold leading-tight">About</h3>
+        <SplitSection
+          primarySlot={
+            <>
+              <div className="lg:pr-32 xl:pr-48">
+                <p className="mt-8 text-l font-light leading-relaxed">Zachary Van Tol is the founder of
+                  Happenstance, bringing 5+ years of
+                  experience at the interface of higher
+                  education and partnerships with the
+                  public and private sectors. He holds a
+                  B.S. and M.S. from Virginia Tech and is
+                  a Ph.D. Candidate at Arizona State
+                  University’s School of Sustainability.</p>
+              </div>
+              <div className="lg:pr-32 xl:pr-48">
+                <p className="mt-8 text-l font-light leading-relaxed">Zach founded Happenstance to
+                  seamlessly integrate networking into
+                  everyday life, believing technology
+                  should enhance, not replace,
+                  in-person communication.</p>
+              </div>
+            </>
+          }
+          secondarySlot={
+            <>
+              <div>
+                <img width={"100%"} src={'/trees.png'} />
+              </div>
+            </>}
+        />
+
       </div>
     </section>
     <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
